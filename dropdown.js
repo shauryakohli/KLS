e98dc1882b1d931d10d2060f907fee854dcfecbd
Shaurya Kohli <shauryakohli17@gmail.com>
2023-07-27 15:00:11 +0530

    Add files via upload

--- dropdown.js
+++ dropdown.js
@@ -1,12 +1,22 @@
 const dropdown = document.querySelector(".dropdown");
 const dropdownContent = document.querySelector(".dropdown-content");
+let timeoutId;
 
-// Add event listener to show the content on hover
-dropdown.addEventListener("mouseenter", function () {
+// Function to show the dropdown content
+function showDropdownContent() {
+  clearTimeout(timeoutId);
   dropdownContent.style.display = "block";
-});
+}
 
-// Add event listener to hide the content when the mouse leaves the menu
-dropdown.addEventListener("mouseleave", function () {
-  dropdownContent.style.display = "none";
-});
+// Function to hide the dropdown content with a brief delay
+function hideDropdownContent() {
+  timeoutId = setTimeout(() => {
+    dropdownContent.style.display = "none";
+  }, 600); // Adjust the delay time (in milliseconds) as needed
+}
+
+// Add event listeners for hover events
+dropdown.addEventListener("mouseenter", showDropdownContent);
+dropdown.addEventListener("mouseleave", hideDropdownContent);
+dropdownContent.addEventListener("mouseenter", showDropdownContent);
+dropdownContent.addEventListener("mouseleave", hideDropdownContent);
