19e44099cfd8fef89c094d27c775a9bea6d22e26
Shaurya Kohli <shauryakohli17@gmail.com>
2023-07-29 02:46:46 +0530

    Add files via upload

--- contact.js
+++ contact.js
@@ -1,39 +1,35 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const customModal = document.getElementById("customModal");
-  const contactLink = document.getElementById("contactLink");
-  let timer;
+const contactLink = document.getElementById("contactLink");
+const customModal = document.getElementById("customModal");
+const closeBtn = document.getElementById("closeModal");
+let hoverTimeoutId;
 
-  // Function to display the pop-up (modal)
-  function displayModal() {
-    customModal.style.display = "block";
-    clearTimeout(timer); // Clear the previous timer (if any)
-  }
+// Function to show the contact box on hover
+contactLink.addEventListener("mouseenter", function () {
+  customModal.style.display = "block";
+});
 
-  // Function to hide the pop-up (modal)
-  function hideModal() {
+// Function to hide the contact box when the mouse leaves the link
+contactLink.addEventListener("mouseleave", function () {
+  clearTimeout(hoverTimeoutId);
+  hoverTimeoutId = setTimeout(() => {
+    customModal.style.display = "none";
+  }, 20000); // Adjust the delay time (in milliseconds) as needed
+});
+
+// Function to close the contact box when clicking the close button (x)
+closeBtn.addEventListener("click", function () {
+  customModal.style.display = "none";
+});
+
+// Function to close the contact box when clicking outside the modal content
+window.addEventListener("click", function (event) {
+  if (event.target === customModal) {
     customModal.style.display = "none";
   }
-
-  // Add mouseenter and mouseleave event listeners
-  contactLink.addEventListener("mouseenter", function () {
-    // Show the modal when the user hovers over the link
-    displayModal();
-  });
-
-  contactLink.addEventListener("mouseleave", function () {
-    // Hide the modal when the user moves the mouse away from the link
-    // Set a timer to hide the modal after 1 second (1000 milliseconds)
-    timer = setTimeout(hideModal, 3500);
-  });
-
-  // Close the pop-up when clicking the close button (x)
-  const closeModalBtn = document.getElementById("closeModal");
-  closeModalBtn.addEventListener("click", hideModal);
-
-  // Close the pop-up when clicking outside the modal content
-  window.addEventListener("click", function (event) {
-    if (event.target === customModal) {
-      hideModal();
-    }
-  });
 });
+
+
+
+
+
+
